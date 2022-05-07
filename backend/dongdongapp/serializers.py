4cab96dbaa34f9d6de69e114cf4ec96f533954c0
김송이 <ksl2950@o.cnu.ac.kr>
2022-05-07 16:44:01 +0900

    feat : JWT token 발급

--- backend/dongdongapp/serializers.py
+++ backend/dongdongapp/serializers.py
@@ -1,7 +1,10 @@
 from rest_framework import  serializers
 from .models import CustomUser
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework import permissions
 
-
+# 사용자 정보 
 class UserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         user = super().create(validated_data)
@@ -13,3 +16,32 @@ class UserSerializer(serializers.ModelSerializer):
         model = CustomUser
         fields = ('id','username','password')
         extra_kwargs = {'password': {'write_only': True}}
+
+        
+# jwt token 결과 커스텀 
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    
+    # response 커스텀 
+    default_error_messages = {
+        'no_active_account': {'message':'username or password is incorrect!',
+                              'success': False,
+                              'status' : 401}
+    }
+        
+    def validate(self, attrs):
+        data = super().validate(attrs)
+        
+        refresh = self.get_token(self.user)
+        
+         # Add extra responses here
+        data['username'] = self.user.username
+        data['refresh'] = str(refresh)
+        data['access'] = str(refresh.access_token)
+        data['success'] = True
+        
+        return data
+
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    permission_classes = (permissions.AllowAny,)
+    serializer_class = MyTokenObtainPairSerializer
--- backend/dongdongapp/urls.py
+++ backend/dongdongapp/urls.py
@@ -1,8 +1,22 @@
 from django.urls import path, include
 from .views import user_views
+from .serializers import MyTokenObtainPairView
+from rest_framework_simplejwt.views import TokenVerifyView
+from rest_framework_simplejwt.views import (
+    TokenRefreshView,
+    TokenVerifyView
+)
 
 urlpatterns = [
+    
+    # 사용자 조회
     path('users/all',user_views.UserList.as_view()),
     path('users',user_views.CreateUser.as_view()),
-    path('users/<int:pk>',user_views.UserDetail.as_view())
+    path('users/<int:pk>',user_views.UserDetail.as_view()),
+    
+    # 로그인 (JWT TOKEN)
+    path('token', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/verify', TokenVerifyView.as_view(), name='token_verify'),
+    
 ]
