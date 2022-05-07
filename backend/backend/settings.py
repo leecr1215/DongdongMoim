47837f5010bec0cb7e7973173f3d2a3069884bc2
김송이 <ksl2950@o.cnu.ac.kr>
2022-05-07 11:35:44 +0900

    feat : apply restframework url

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'dongdongapp'
+    'dongdongapp',
+    'rest_framework',
+    'rest_framework_simplejwt',
 ]
 
 MIDDLEWARE = [
@@ -124,6 +126,4 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-ROOT_URLCONF = 'backend.urls'
 AUTH_USER_MODEL = "dongdongapp.CustomUser"
