79fb4d6ce903b1d70c0197d4e73759048955e125
leecr1215 <leecr1215@naver.com>
2022-05-28 19:08:51 +0900

    feat: change css gender button, feat: change question css, feat: add modal contents

--- frontend/src/Signup.js
+++ frontend/src/Signup.js
@@ -10,7 +10,9 @@ import {
   KeyboardAvoidingView,
 } from "react-native";
 import { Image } from "react-native";
+import styled from "styled-components/native";
 import axios from "axios";
+import ExerciseModal from "../contents/ExerciseModal";
 
 const { width: SCREEN_WIDTH } = Dimensions.get("window");
 const { height: SCREEN_HEIGHT } = Dimensions.get("window");
@@ -25,6 +27,7 @@ export default function Home({ navigation }) {
   const [baseball, setBaseball] = useState(0);
   const [badminton, setBadminton] = useState(0);
 
+  const [isModalClick, setModalClick] = useState(false);
   const [isGenderSelect, setGenderSelect] = useState([false, false]);
 
   const [isSoccerSelect, setSoccerSelect] = useState([
@@ -45,6 +48,13 @@ export default function Home({ navigation }) {
     false,
     false,
   ]);
+
+  //최외각에서 뷰들을 감싸는 Constainer
+  const StyledSafeAreaView = styled.SafeAreaView`
+    flex: 1;
+    justify-content: center;
+    align-items: center;
+  `;
 
   const onPress = async () => {
     //navigation.navigate("Home");
@@ -90,6 +100,14 @@ export default function Home({ navigation }) {
   //   }
   //   setPhoneNum(phnum);
   // };
+  const onPressQuestion = async () => {
+    setModalClick(true);
+  };
+
+  const onPressModalClose = async () => {
+    setModalClick(false);
+  };
+
   const onPressGender = async (gender) => {
     if (gender === "남") {
       setGender("M");
@@ -116,12 +134,46 @@ export default function Home({ navigation }) {
     }
   };
 
+  const onPressBaseball = async (name) => {
+    if (name === "sole") {
+      setBaseball(1);
+      setBaseballSelect([true, false, false, false]);
+    } else if (name === "sock") {
+      setBaseball(2);
+      setBaseballSelect([false, true, false, false]);
+    } else if (name === "slipper") {
+      setBaseball(3);
+      setBaseballSelect([false, false, true, false]);
+    } else if (name === "sneaker") {
+      setBaseball(4);
+      setBaseballSelect([false, false, false, true]);
+    }
+  };
+
+  const onPressBadmin = async (name) => {
+    if (name === "sole") {
+      setBadminton(1);
+      setBadminSelect([true, false, false, false]);
+    } else if (name === "sock") {
+      setBadminton(2);
+      setBadminSelect([false, true, false, false]);
+    } else if (name === "slipper") {
+      setBadminton(3);
+      setBadminSelect([false, false, true, false]);
+    } else if (name === "sneaker") {
+      setBadminton(4);
+      setBadminSelect([false, false, false, true]);
+    }
+  };
+
   return (
-    <KeyboardAvoidingView style={styles.container}>
+    <StyledSafeAreaView style={styles.container}>
+      <ExerciseModal isVisible={isModalClick} isClose={onPressModalClose} />
       <StatusBar style="auto" />
       <View style={styles.head}>
         <Text style={styles.appText}>회원가입</Text>
       </View>
+
       <View style={styles.signUp}>
         <TextInput
           style={styles.input}
@@ -187,49 +239,41 @@ export default function Home({ navigation }) {
         <View style={styles.exerciseContainer}>
           <View style={styles.exerciseTextContainer}>
             <Text style={styles.exerciseText}>운동능력</Text>
-            <TouchableOpacity>
+            <TouchableOpacity onPress={() => onPressQuestion()}>
               <Text style={styles.question}>?</Text>
             </TouchableOpacity>
           </View>
           {/* 축구 부분 */}
           <View style={styles.exercises}>
             <Text style={styles.soccer}>축구</Text>
-            <View
-              style={isSoccerSelect[0] ? styles.pressBtn : styles.imageStyle}
-            >
+            <View style={styles.imageStyle}>
               <TouchableOpacity onPress={() => onPressSoccer("sole")}>
                 <Image
-                  style={styles.logo}
+                  style={isSoccerSelect[0] ? styles.pressBtn : styles.logo}
                   source={require("../icon/sole.png")}
                 />
               </TouchableOpacity>
             </View>
-            <View
-              style={isSoccerSelect[1] ? styles.pressBtn : styles.imageStyle}
-            >
+            <View style={styles.imageStyle}>
               <TouchableOpacity onPress={() => onPressSoccer("sock")}>
                 <Image
-                  style={styles.logo}
+                  style={isSoccerSelect[1] ? styles.pressBtn : styles.logo}
                   source={require("../icon/sock.png")}
                 />
               </TouchableOpacity>
             </View>
-            <View
-              style={isSoccerSelect[2] ? styles.pressBtn : styles.imageStyle}
-            >
+            <View style={styles.imageStyle}>
               <TouchableOpacity onPress={() => onPressSoccer("slipper")}>
                 <Image
-                  style={styles.logo}
+                  style={isSoccerSelect[2] ? styles.pressBtn : styles.logo}
                   source={require("../icon/slipper.png")}
                 />
               </TouchableOpacity>
             </View>
-            <View
-              style={isSoccerSelect[3] ? styles.pressBtn : styles.imageStyle}
-            >
+            <View style={styles.imageStyle}>
               <TouchableOpacity onPress={() => onPressSoccer("sneaker")}>
                 <Image
-                  style={styles.logo}
+                  style={isSoccerSelect[3] ? styles.pressBtn : styles.logo}
                   source={require("../icon/sneaker.png")}
                 />
               </TouchableOpacity>
@@ -238,10 +282,74 @@ export default function Home({ navigation }) {
           {/* 야구 부분 */}
           <View style={styles.exercises}>
             <Text style={styles.baseball}>야구</Text>
+            <View style={styles.imageStyle}>
+              <TouchableOpacity onPress={() => onPressBaseball("sole")}>
+                <Image
+                  style={isBaseballSelect[0] ? styles.pressBtn : styles.logo}
+                  source={require("../icon/sole.png")}
+                />
+              </TouchableOpacity>
+            </View>
+            <View style={styles.imageStyle}>
+              <TouchableOpacity onPress={() => onPressBaseball("sock")}>
+                <Image
+                  style={isBaseballSelect[1] ? styles.pressBtn : styles.logo}
+                  source={require("../icon/sock.png")}
+                />
+              </TouchableOpacity>
+            </View>
+            <View style={styles.imageStyle}>
+              <TouchableOpacity onPress={() => onPressBaseball("slipper")}>
+                <Image
+                  style={isBaseballSelect[2] ? styles.pressBtn : styles.logo}
+                  source={require("../icon/slipper.png")}
+                />
+              </TouchableOpacity>
+            </View>
+            <View style={styles.imageStyle}>
+              <TouchableOpacity onPress={() => onPressBaseball("sneaker")}>
+                <Image
+                  style={isBaseballSelect[3] ? styles.pressBtn : styles.logo}
+                  source={require("../icon/sneaker.png")}
+                />
+              </TouchableOpacity>
+            </View>
           </View>
           {/* 배드민턴 부분 */}
           <View style={styles.exercises}>
             <Text style={styles.badminton}>배드민턴</Text>
+            <View style={styles.imageStyle}>
+              <TouchableOpacity onPress={() => onPressBadmin("sole")}>
+                <Image
+                  style={isBadminSelect[0] ? styles.pressBtn : styles.logo}
+                  source={require("../icon/sole.png")}
+                />
+              </TouchableOpacity>
+            </View>
+            <View style={styles.imageStyle}>
+              <TouchableOpacity onPress={() => onPressBadmin("sock")}>
+                <Image
+                  style={isBadminSelect[1] ? styles.pressBtn : styles.logo}
+                  source={require("../icon/sock.png")}
+                />
+              </TouchableOpacity>
+            </View>
+            <View style={styles.imageStyle}>
+              <TouchableOpacity onPress={() => onPressBadmin("slipper")}>
+                <Image
+                  style={isBadminSelect[2] ? styles.pressBtn : styles.logo}
+                  source={require("../icon/slipper.png")}
+                />
+              </TouchableOpacity>
+            </View>
+            <View style={styles.imageStyle}>
+              <TouchableOpacity onPress={() => onPressBadmin("sneaker")}>
+                <Image
+                  style={isBadminSelect[3] ? styles.pressBtn : styles.logo}
+                  source={require("../icon/sneaker.png")}
+                />
+              </TouchableOpacity>
+            </View>
           </View>
         </View>
 
@@ -249,7 +357,7 @@ export default function Home({ navigation }) {
           <Text style={styles.signUpBtn}>확인</Text>
         </TouchableOpacity>
       </View>
-    </KeyboardAvoidingView>
+    </StyledSafeAreaView>
   );
 }
 
@@ -295,7 +403,8 @@ const styles = StyleSheet.create({
   genderText: {
     fontSize: 15,
     fontWeight: "700",
-    marginTop: SCREEN_HEIGHT * 0.01,
+    marginTop: SCREEN_HEIGHT * 0.02,
+    marginBottom: SCREEN_HEIGHT * 0.02,
   },
   genderBtn: {
     backgroundColor: "#F3F3F3",
@@ -308,8 +417,7 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.3,
     shadowOffset: { width: 2, height: 2 },
     elevation: 3,
-    color: "#9C9C9C",
-    marginBottom: SCREEN_HEIGHT * 0.03,
+    marginBottom: SCREEN_HEIGHT * 0.05,
   },
   ageContainer: {
     flexDirection: "row",
@@ -322,9 +430,9 @@ const styles = StyleSheet.create({
     borderRightWidth: 0,
     borderTopWidth: 0,
     borderBottomWidth: 1,
-    marginBottom: SCREEN_HEIGHT * 0.03,
     width: SCREEN_WIDTH * 0.3,
     borderColor: "#9C9C9C",
+    marginBottom: SCREEN_HEIGHT * 0.05,
   },
   phoneContainer: {
     flexDirection: "row",
@@ -346,6 +454,7 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     fontWeight: "700",
     justifyContent: "space-between",
+    marginBottom: SCREEN_HEIGHT * 0.02,
   },
   exerciseText: { fontSize: 15, fontWeight: "700" },
   question: {
@@ -354,39 +463,57 @@ const styles = StyleSheet.create({
     height: 20,
     borderWidth: 1,
     borderRadius: 20 / 2,
-    backgroundColor: "#DFDFDF",
-    borderColor: "#DFDFDF",
+    backgroundColor: "black",
+    color: "white",
+    borderColor: "black",
   },
-  exercises: { flexDirection: "row" },
+  exercises: { flexDirection: "row", marginBottom: SCREEN_HEIGHT * 0.02 },
   soccer: {
-    fontSize: 13,
+    fontSize: 12,
     color: "#898989",
-    marginRight: SCREEN_WIDTH * 0.09,
+    marginRight: SCREEN_WIDTH * 0.08,
+  },
+  baseball: {
+    fontSize: 12,
+    color: "#898989",
+    marginRight: SCREEN_WIDTH * 0.08,
+  },
+  badminton: {
+    fontSize: 12,
+    color: "#898989",
+    marginRight: SCREEN_WIDTH * 0.013,
+    marginBottom: SCREEN_HEIGHT * 0.05,
   },
   logo: {
     resizeMode: "contain",
     height: SCREEN_HEIGHT * 0.025,
     width: SCREEN_WIDTH * 0.08,
     alignContent: "center",
+    borderColor: "#898989",
+    backgroundColor: "#FBE573",
+    borderWidth: 1,
+  },
+  pressBtn: {
+    resizeMode: "contain",
+    height: SCREEN_HEIGHT * 0.025,
+    width: SCREEN_WIDTH * 0.08,
+    alignContent: "center",
+    borderColor: "#FBE573",
+    borderWidth: 1,
   },
   imageStyle: {
-    borderColor: "#898989",
-    borderWidth: 1,
+    //borderColor: "#898989",
+    //borderWidth: 1,
     alignContent: "center",
+    justifyContent: "space-between",
+    width: SCREEN_WIDTH * 0.1,
+    //height: SCREEN_HEIGHT * 0.03,
+    //width: SCREEN_WIDTH * 0.085,
     // height: SCREEN_HEIGHT * 0.028,
     // width: SCREEN_WIDTH * 0.085,
-    marginLeft: SCREEN_WIDTH * 0.01,
+    //marginLeft: SCREEN_WIDTH * 0.01,
   },
-  baseball: {
-    fontSize: 13,
-    color: "#898989",
-    marginRight: SCREEN_WIDTH * 0.09,
-  },
-  badminton: {
-    fontSize: 13,
-    color: "#898989",
-    marginRight: SCREEN_WIDTH * 0.09,
-  },
+
   signUpBtn: {
     backgroundColor: "#D3EEFF",
     fontSize: 17,
@@ -402,7 +529,7 @@ const styles = StyleSheet.create({
     elevation: 3,
   },
   genderPressBtn: {
-    backgroundColor: "#F3F3F3",
+    backgroundColor: "#FBE573",
     fontSize: 15,
     textAlign: "center",
     paddingBottom: 8,
@@ -415,13 +542,6 @@ const styles = StyleSheet.create({
     color: "black",
     borderColor: "#FBE573",
     borderWidth: 1,
-    marginBottom: SCREEN_HEIGHT * 0.03,
-  },
-  pressBtn: {
-    borderColor: "#FBE573",
-    borderWidth: 1,
-    height: SCREEN_HEIGHT * 0.03,
-    width: SCREEN_WIDTH * 0.08,
-    marginLeft: SCREEN_WIDTH * 0.01,
+    marginBottom: SCREEN_HEIGHT * 0.05,
   },
 });
