ea02739283a5689c1283e9f34851c76e14601149
leecr1215 <leecr1215@naver.com>
2022-06-06 22:09:39 +0900

    feat: change search css and get post

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,4 +1,3 @@
-import { StatusBar } from "expo-status-bar";
 import {
   StyleSheet,
   Text,
@@ -7,14 +6,20 @@ import {
   Dimensions,
   TextInput,
   ScrollView,
+  StatusBar,
+  Platform,
 } from "react-native";
 import { Image } from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useEffect, useState } from "react";
 import Header from "../contents/Header";
+import axios from "axios";
 //import DropDownPicker from "react-native-dropdown-picker";
 import { Picker } from "@react-native-picker/picker";
 import { Dropdown } from "react-native-element-dropdown";
+import Constants from "expo-constants";
+
+const { manifest } = Constants;
 
 const { width: SCREEN_WIDTH } = Dimensions.get("window");
 const { height: SCREEN_HEIGHT } = Dimensions.get("window");
@@ -28,19 +33,52 @@ export default function Home({ navigation }) {
   const [selectedSkill, setSelectedSkill] = useState(null);
   const [selectedExercise, setSelectedExercise] = useState(null);
 
+  const [postData, setPostData] = useState(null);
+  const [postCheck, setPostCheck] = useState(false);
   const [dropdown, setDropdown] = useState(null);
+  const setData = async (data) => {
+    try {
+      await setPostData(data);
+      await setPostCheck(true);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  useEffect(() => {
+    async function getData() {
+      try {
+        const response = await axios
+          .get(
+            `http://${manifest.debuggerHost
+              .split(":")
+              .shift()}:8080/api/v1/posts/all`
+          )
+          .then((response) => {
+            if (response.data["success"] == true) {
+              const data = response.data["data"];
+              setData(data);
 
-  // useEffect(() => {
-  //   async function Data() {
-  //     const storage1 = await AsyncStorage.getItem("@username").then((name) =>
-  //       setUsername(name.slice(1, -1))
-  //     );
-  //     const storage2 = await AsyncStorage.getItem("@id").then(
-  //       (userid) => setIdNum(userid.slice(1, -1)),
-  //       console.log(idNum + " 님 하이")
-  //     );
-  //   }
-  // }, []);
+              console.log("난 data");
+              console.log(data);
+
+              //console.log(response.data["data"]);
+              //console.log(postData);
+              //console.log(response.data["data"][1]["age"]);
+            }
+          })
+          .catch(function (error) {
+            alert("게시물을 가져오지 못 했습니다.");
+            console.log(error);
+            throw error;
+          });
+      } catch (error) {
+        alert("게시물을 가져오지 못 했습니다.");
+        console.log(error);
+        throw error;
+      }
+    }
+    getData();
+  }, [setPostData, StatusBar]);
 
   const storage1 = AsyncStorage.getItem("@username").then((name) =>
     setUsername(name.slice(1, -1))
@@ -86,85 +124,122 @@ export default function Home({ navigation }) {
       </View>
     );
   };
+
   return (
     <View style={styles.container}>
+      <StatusBar backgroundColor={"#D3EEFF"} />
       <Header navigation={navigation}></Header>
+      <View style={styles.search}>
+        <Dropdown
+          style={styles.dropdown}
+          containerStyle={styles.shadow}
+          data={ageItems}
+          labelField="label"
+          valueField="value"
+          label="Dropdown"
+          placeholder="연령"
+          value={selectedAge}
+          onChange={(item) => {
+            setSelectedAge(item.value);
+            console.log("selected", item);
+          }}
+          autoScroll
+          renderItem={(item) => _renderItem(item)}
+          textError="Error"
+          placeholderStyle={styles.selectedText}
+          selectedTextStyle={styles.selectedText}
+        />
+        <Dropdown
+          style={styles.dropdown}
+          containerStyle={styles.shadow}
+          data={genderItems}
+          labelField="label"
+          valueField="value"
+          label="Dropdown"
+          placeholder="성별"
+          value={selectedGender}
+          onChange={(item) => {
+            setSelectedGender(item.value);
+            console.log("selected", item);
+          }}
+          renderItem={(item) => _renderItem(item)}
+          textError="Error"
+          placeholderStyle={styles.selectedText}
+          selectedTextStyle={styles.selectedText}
+        />
+        <Dropdown
+          style={styles.dropdown}
+          containerStyle={styles.shadow}
+          data={skillItems}
+          labelField="label"
+          valueField="value"
+          label="Dropdown"
+          placeholder="능력"
+          value={selectedSkill}
+          onChange={(item) => {
+            setSelectedSkill(item.value);
+            console.log("selected", item);
+          }}
+          renderItem={(item) => _renderItem(item)}
+          textError="Error"
+          placeholderStyle={styles.selectedText}
+          selectedTextStyle={styles.selectedText}
+        />
+        <Dropdown
+          style={styles.dropdown}
+          containerStyle={styles.shadow}
+          data={exerItems}
+          labelField="label"
+          valueField="value"
+          label="Dropdown"
+          placeholder="운동"
+          value={selectedExercise}
+          autoScroll
+          onChange={(item) => {
+            setSelectedExercise(item.value);
+            console.log("selected", item);
+          }}
+          renderItem={(item) => _renderItem(item)}
+          textError="Error"
+          placeholderStyle={styles.selectedText}
+          selectedTextStyle={styles.selectedText}
+        />
+        <TouchableOpacity onPress={() => navigation.navigate("PostWriting")}>
+          <Image
+            style={styles.postWriteLogo}
+            source={require("../icon/post_write.png")}
+          />
+        </TouchableOpacity>
+      </View>
       <View style={styles.body}>
-        <View style={styles.search}>
-          <Dropdown
-            style={styles.dropdown}
-            containerStyle={styles.shadow}
-            data={ageItems}
-            labelField="label"
-            valueField="value"
-            label="Dropdown"
-            placeholder="연령"
-            value={selectedAge}
-            onChange={(item) => {
-              setSelectedAge(item.value);
-              console.log("selected", item);
-            }}
-            renderItem={(item) => _renderItem(item)}
-            textError="Error"
-          />
-          <Dropdown
-            style={styles.dropdown}
-            containerStyle={styles.shadow}
-            data={genderItems}
-            labelField="label"
-            valueField="value"
-            label="Dropdown"
-            placeholder="성별"
-            value={selectedGender}
-            onChange={(item) => {
-              setSelectedGender(item.value);
-              console.log("selected", item);
-            }}
-            renderItem={(item) => _renderItem(item)}
-            textError="Error"
-          />
-          <Dropdown
-            style={styles.dropdown}
-            containerStyle={styles.shadow}
-            data={skillItems}
-            labelField="label"
-            valueField="value"
-            label="Dropdown"
-            placeholder="능력"
-            value={selectedSkill}
-            onChange={(item) => {
-              setSelectedSkill(item.value);
-              console.log("selected", item);
-            }}
-            renderItem={(item) => _renderItem(item)}
-            textError="Error"
-          />
-          <Dropdown
-            style={styles.dropdown}
-            containerStyle={styles.shadow}
-            data={exerItems}
-            labelField="label"
-            valueField="value"
-            label="Dropdown"
-            placeholder="운동"
-            value={selectedExercise}
-            autoScroll
-            onChange={(item) => {
-              setSelectedExercise(item.value);
-              console.log("selected", item);
-            }}
-            renderItem={(item) => _renderItem(item)}
-            textError="Error"
-          />
-          <TouchableOpacity onPress={() => navigation.navigate("PostWriting")}>
-            <Image
-              style={styles.postWriteLogo}
-              source={require("../icon/post_write.png")}
-            />
-          </TouchableOpacity>
-        </View>
         <View style={styles.postContainer}>
-          <ScrollView></ScrollView>
+          <ScrollView style={styles.scrollView} persistentScrollbar={true}>
+            {postCheck ? (
+              postData.map((post, index) => (
+                <View style={styles.scrollChild}>
+                  <View style={styles.postIdDate}>
+                    <Text key={post["post_date"]}>{post["post_id"]}</Text>
+                    <Text key={index * post["post_id"] + 2}>
+                      {post["post_date"].split(" ")[0]}
+                    </Text>
+                  </View>
+                  <View style={styles.postTitleNumber}>
+                    <Text key={index * post["post_id"] + 3}>
+                      {post["title"]}
+                    </Text>
+                    <View>
+                      <Text key={index * post["post_id"] + 4}>신청완료</Text>
+                      <Text key={index * post["post_id"] + 5}>
+                        {post["required_number"]}
+                      </Text>
+                    </View>
+                  </View>
+                </View>
+              ))
+            ) : (
+              <></>
+            )}
+          </ScrollView>
         </View>
       </View>
     </View>
@@ -176,23 +251,25 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#F3F3F3",
   },
-  body: { flex: 0.87 },
+  body: {
+    flex: 0.87,
+  },
   search: {
-    width: SCREEN_WIDTH,
+    width: SCREEN_WIDTH * 0.9,
     height: SCREEN_HEIGHT * 0.05,
     flexDirection: "row",
-    justifyContent: "space-around",
+    justifyContent: "space-between",
     alignItems: "center",
+    alignContent: "center",
     marginTop: SCREEN_WIDTH * 0.05,
-    borderColor: "black",
-    borderWidth: 3,
+    marginLeft: SCREEN_WIDTH * 0.05,
+    marginRight: SCREEN_WIDTH * 0.05,
   },
   /*드롭다운 css 시작*/
   dropdown: {
-    //backgroundColor: "white",
-    //borderBottomColor: "gray",
-    //borderBottomWidth: 0.5,
-    //marginTop: 20,
+    backgroundColor: "white",
+    borderBottomColor: "gray",
+    borderBottomWidth: 0.5,
     width: SCREEN_WIDTH * 0.15,
   },
 
@@ -202,21 +279,27 @@ const styles = StyleSheet.create({
     //flexDirection: "row",
     //justifyContent: "center",
     alignItems: "center",
+    fontSize: 14,
   },
   textItem: {
     flex: 1,
-    fontSize: 15,
+    fontSize: 14,
   },
   shadow: {
     margin: 0,
+    maxHeight: 100,
     shadowColor: "#000",
     shadowOffset: {
       width: 0,
-      height: 1,
+      height: 2,
     },
-    shadowOpacity: 0.2,
+    shadowOpacity: 0.5,
     shadowRadius: 1.41,
-    elevation: 2,
+    elevation: 4,
+  },
+  selectedText: {
+    fontSize: 14,
+    textAlign: "center",
   },
   /*드롭다운 css 끝*/
   postWriteLogo: { resizeMode: "contain", width: SCREEN_WIDTH * 0.06 },
@@ -230,4 +313,14 @@ const styles = StyleSheet.create({
     marginRight: "auto",
     marginLeft: "auto",
   },
+  scrollView: {
+    width: SCREEN_WIDTH * 0.9,
+    //height: SCREEN_HEIGHT * 0.74,
+  },
+  scrollChild: {
+    borderBottomColor: "#dcdcdc",
+    borderBottomWidth: 1,
+  },
+  postIdDate: {},
+  postTitleNumber: {},
 });
