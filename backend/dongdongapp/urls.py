1b524ffe8b430656ac84935b7ab58aa1682e8642
김송이 <ksl2950@naver.com>
2022-06-10 17:41:56 +0900

    feat : 친구 신청 내역 조회

--- backend/dongdongapp/urls.py
+++ backend/dongdongapp/urls.py
@@ -41,7 +41,7 @@ urlpatterns = [
          friend_views.FriendDetail.as_view()),
     path('v1/friends/connection/<int:user1>/<int:user2>',
          friend_views.FriendDetail3.as_view()),
-
+    path('v1/friends/application/<int:user1>',friend_views.FriendDetail4.as_view()),
 
     # 게시글 신청
     path('v1/posts/applicants', postApplication_views.PostApplicantsList.as_view()),
